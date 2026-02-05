66572fd6d549a0fca8e03de62c5f1de197c0d24d
2600box <6595566+2600box@users.noreply.github.com>
2026-02-05 18:23:27 +0100

    Remove RequireDotView from Subsonic client config

--- cmd/itunes2subsonic.go
+++ cmd/itunes2subsonic.go
@@ -296,7 +296,6 @@ func main() {
 		BaseUrl:        *subsonicUrl,
 		User:           subsonicUser,
 		ClientName:     "apple-music2subsonic",
-		RequireDotView: true,
 	}
 	if err := c.Authenticate(subsonicPass); err != nil {
 		log.Fatalf("Failed to create Subsonic client: %s", err)
